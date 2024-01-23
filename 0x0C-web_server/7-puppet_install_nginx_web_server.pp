9d6567cd3c4028036dcf33eed593e69840f5a583
josephorokpo <josephorokpo@gmail.com>
2024-01-23 05:43:20 -0800

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Puppet manifest containing commands to automatically configure an Ubuntu machine
+
+package { 'inginx':
+  ensure => installed,
+}
+file_line {'install':
+  ensure => 'present',
+  path	 => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/josephorokpo permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require =>  package{'nginx'},
+}
