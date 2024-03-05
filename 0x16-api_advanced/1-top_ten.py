a5b8d9f7a01fc5e729a3e15c782a30692a062e3e
josephorokpo <josephorokpo@gmail.com>
2024-03-05 03:06:53 -0800

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module provides a function to query the Reddit API and print the
-titles of the first 10 hot posts listed for a given subreddit.
+This module provides a function to query the Reddit API and print the titles
+of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
@@ -18,9 +18,11 @@ def top_ten(subreddit):
     Returns:
         None
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
+    params = {'limit': 10}  # Request 10 posts
+
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
         data = response.json()
@@ -29,3 +31,9 @@ def top_ten(subreddit):
             print(post['data']['title'])
     else:
         print(None)
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter subreddit name: ")
+    print("Top 10 hot posts:")
+    top_ten(subreddit)
