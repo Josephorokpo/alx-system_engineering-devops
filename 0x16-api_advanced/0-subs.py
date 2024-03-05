1b0f8a412a3fe626d39d46822dcd707634f3d1e9
josephorokpo <josephorokpo@gmail.com>
2024-03-05 03:19:27 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,17 +6,26 @@ Contains the number_of_subscribers function
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+def number_of_subscribers(subreddit_name):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit_name (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
+
+    if subreddit_name is None or type(subreddit_name) is not str:
         return 0
 
     response = requests.get(
-        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+        'http://www.reddit.com/r/{}/about.json'.format(subreddit_name),
         headers={
             'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
         }
     ).json()
 
-    subscribers = response.get("data", {}).get("subscribers", 0)
-    return subscribers
+    subscribers_count = response.get("data", {}).get("subscribers", 0)
+    return subscribers_count
