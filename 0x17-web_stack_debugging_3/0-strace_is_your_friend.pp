78da44ba6c2aa0ca13197b153867709ac7ce364f
josephorokpo <josephorokpo@gmail.com>
2024-03-06 15:14:10 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to fix bad "phpp" extensions to "php" in wp-settings.php
+# Puppet manifest to fix bad "phpp" extensions to "php" in "wp-settings.php"
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
