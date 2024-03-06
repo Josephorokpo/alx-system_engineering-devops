a84c17929d0f0bde0d16c06d9ee352ba4ae2d747
josephorokpo <josephorokpo@gmail.com>
2024-03-06 04:30:43 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
 # Puppet manifest to fix bad `phpp` extensions to `php` in wp-settings.php
 
-file { '/path/to/wp-settings.php':
-  ensure  => file,
-  content => template('path/to/wp-settings.php.erb'),
-  notify  => Exec['fix_phpp_extension'],
-}
-
-exec { 'fix_phpp_extension':
-  command     => "/bin/sed -i 's/phpp/php/g' /path/to/wp-settings.php",
-  refreshonly => true,
-  subscribe   => File['/path/to/wp-settings.php'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
